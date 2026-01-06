702e7664a8299746e3eebfdafbbbbf54564c265b
pradipsinh <pradipsinh0906@gmail.com>
2026-01-06 12:54:29 +0530

    Oprations learening.

--- if.py
+++ if.py
@@ -7,9 +7,32 @@
 # print(f"Hello, {name}! \n You are {age} years old.")
 
 
-number1 = int(input("Enter your number1: "))
-number2 = int(input("Enter your number2: "))
+# number1 = int(input("Enter your number1: "))
+# number2 = int(input("Enter your number2: "))
 
-print(f"Addition: {number1 + number2}\n" f"Subtraction: {number1 - number2}\n" f"Multiplication: {number1 * number2}\n" f"Division: {number1 / number2}")
+# print(f"Addition: {number1 + number2}\n" f"Subtraction: {number1 - number2}\n" f"Multiplication: {number1 * number2}\n" f"Division: {number1 / number2}")
 
-print("Hello World")
+# print("Hello World")
+
+     
+a = "vivek pradip  meetraj"
+
+print(a[3:11:-1])
+print(len(a))
+
+
+# a = 5
+# b= 32
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(b / a)
+# print(b // a)
+# print(5 ** 3)
+# print(50 % 3)
+
+# print(12 + 4 - 56 * 2 / 2)
+
+# * compound assignment operators
+print("Apple" <  "bAnana")
---
+++ operations.py
@@ -0,0 +1,108 @@
+"""variables"""
+# sher = "harsh bhaiya"
+# School = "students" #pascal case
+# School = "students" #camel case
+
+"""data types"""
+# a = -34
+# b = 56.8
+# c = 12/3
+# v = 34j
+
+# print(type(v))
+# b=34
+
+# print(type(b))
+# print(type(b))
+# print(type(b))
+# print(type(b))
+
+
+# st = '1231243235 dsagaiogiaeb !@#$%^&*'
+# print(type(st))
+
+# b = True
+# t = False
+# print(type(b))
+
+
+"""strings"""
+a = "vivek pradip  meetraj"
+
+
+print(a[3:8:1])#ek pr
+print(a[3:11:-1])#ikri
+print(len(a))
+
+
+# a = 12 
+
+# print(12/3)
+
+# name = "akarsh"
+# age = "23"
+
+# print(f"my name is {name} and my age is {age}")
+
+# age = int(input("hello what is your age"))
+
+# print(age)
+
+# a = 5
+# b = 32
+
+
+# print(a + b)
+# print(b - a)
+# print(a * b)
+# print(b//a)
+# print(b/a)
+# print(5**3)
+# print(50%5)
+
+
+# print(12+4-56*2/2)
+
+
+#assignment operators 
+
+# a = 23
+
+#compound assignmet operations
+
+# a = 10
+
+# a += 20
+# a += 40
+# a += 60
+# a-=10
+# a*=50
+# a/=12
+# a//=14
+# a**=2
+# print(a)
+
+# a = 12.1
+# b = 12 
+
+
+# print(a == b)
+
+# print(a != b)
+
+# print(a > b)
+# print(45 < 67)
+# print(23 >= 23)
+# print(45 <= 45)
+
+
+# print(ord("A"))
+# print(ord("a"))
+# print(ord("B"))
+# print(ord("b"))
+
+# print("ABC" < "ACD")
+
+# print("A" > "B")
+
+print("Apple" < "bAnana")
