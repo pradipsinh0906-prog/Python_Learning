2bf5ed90e61175eeeaf909e2c7473f5cdde6756a
pradipsinh <pradipsinh0906@gmail.com>
2026-01-06 11:22:35 +0530

    first program in python.

---
+++ if.py
@@ -0,0 +1,14 @@
+# var1 = None
+# print(type(var1))
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print(f"Hello, {name}! \n You are {age} years old.")
+
+
+number1 = int(input("Enter your number1: "))
+number2 = int(input("Enter your number2: "))
+
+print(f"Addition: {number1 + number2}\n" f"Subtraction: {number1 - number2}\n" f"Multiplication: {number1 * number2}\n" f"Division: {number1 / number2}")
+
